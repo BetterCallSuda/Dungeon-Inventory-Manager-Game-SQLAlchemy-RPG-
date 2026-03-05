3a90076200432c82fc5cba91397eb09cb3c682d8
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-05 21:24:19 +0530

    Update Player Table.py

--- Player Table.py
+++ Player Table.py
@@ -1,3 +1,8 @@
 class Player(Base):
     __tablename__ = "players"
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    health = Column(Integer)
+    level = Column(Integer)
+    gold = Column(Integer)
