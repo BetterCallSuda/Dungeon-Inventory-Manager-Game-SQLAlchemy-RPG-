305ee34451c67e2a6515007fcd344c07cf8e330e
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-05 21:25:24 +0530

    Update Monsters Table.py

--- Monsters Table.py
+++ Monsters Table.py
@@ -1,3 +1,7 @@
 class Monster(Base):
     __tablename__ = "monsters"
 
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    damage = Column(Integer)
+    health = Column(Integer)
