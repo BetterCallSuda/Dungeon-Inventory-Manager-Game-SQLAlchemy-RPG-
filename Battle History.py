554263226ae8adefb6e04efaab70864b04ee2a8c
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-05 21:25:48 +0530

    Update Battle History.py

--- Battle History.py
+++ Battle History.py
@@ -1,3 +1,7 @@
 class Battle(Base):
     __tablename__ = "battles"
 
+    id = Column(Integer, primary_key=True)
+    monster_name = Column(String)
+    result = Column(String)
+    player_id = Column(Integer)
