a952ec9cb0685035a672e9717bdf2edd261c6beb
Sudharson Saravanan <160273862+BetterCallSuda@users.noreply.github.com>
2026-03-05 21:24:57 +0530

    Update Inventory Table.py

--- Inventory Table.py
+++ Inventory Table.py
@@ -3,3 +3,6 @@ class Inventory(Base):
     __tablename__ = "inventory"
 
     id = Column(Integer, primary_key=True)
+    item_name = Column(String)
+    quantity = Column(Integer)
+    player_id = Column(Integer, ForeignKey("players.id"))
